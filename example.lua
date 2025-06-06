bd8964d8b1110a85821522274d5304a2308edaf0
takgoo31 <takgoo.official@gmail.com>
2025-06-06 11:53:53 +0800

    Create example.lua
    added Window.

---
+++ example.lua
@@ -0,0 +1,7 @@
+----------- LOADING ---------------
+local Fluent = print("fluent")
+
+local Window = Fluent:CreateWindow({
+    Title = "Kai Hub : Example", -- You can replace this with the title of your script.
+    SubTitle = "by Kai Team", -- You can change this also.
+    
